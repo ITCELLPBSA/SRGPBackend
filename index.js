fd56ff4f48825eddc868223eaa1b71730b836a4b
ITCELLPBSA <100839109+ITCELLPBSA@users.noreply.github.com>
2022-08-13 12:17:37 +0530

    added cors

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ const app = express();
 
 const MAX_UPLOAD_SIZE = '10mb';
 app.use(cors());
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 app.use(bodyParser.json({limit:MAX_UPLOAD_SIZE}));
 app.use(bodyParser.urlencoded({extended:true, limit:MAX_UPLOAD_SIZE}))
